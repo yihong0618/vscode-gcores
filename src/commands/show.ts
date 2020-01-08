308793c543be36cfe11fa20c4a7c8327793b826e
yihong0618 <zouzou0208@gmail.com>
2020-01-08 18:06:48 +0800

    init webview

--- src/commands/show.ts
+++ src/commands/show.ts
@@ -1,8 +1,29 @@
 import * as vscode from "vscode";
-import { getOneArticleData } from "../api"; 
+import { WebviewPanel } from "vscode";
+import { getOneArticleData } from "../api";
+
+function getWebviewContent(content): string {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>1212</title>
+        <style>
+        </style>
+    </head>
+    <body>
+        ${content}
+    </body>
+    </html>`;
+}
+
+function parseContent(content: string): string {
+    return "232"
+}
 
 export async function previewArticle(node): Promise<void> {
-    console.log(node.id)
-    let data = await getOneArticleData(node.id);
-    console.log(data)
+    const articleData: any = await getOneArticleData(node.id);
+    const panel: WebviewPanel | undefined = vscode.window.createWebviewPanel(node.name, node.name, vscode.ViewColumn.One, {});
+    // console.log(eval(articleData.data.attributes.content));
+    panel.webview.html = getWebviewContent(articleData.data.attributes.content);
 }
